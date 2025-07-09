6d2614aa5f64632309faa7d484476a383f98476a
Aidan Isner <54968155+IsnerA@users.noreply.github.com>
2025-07-09 19:46:36 -0400

    Implement finite state machine for enemy AI

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,19 +1,34 @@
+using System;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace Game.Enemy
 {
+    public interface IState
+    {
+        public void Enter();
+        public void Update();
+        public void Exit();
+    }
+        
     public class EnemyController : MonoBehaviour
     {
         public NavMeshAgent Agent { get; private set; }
-        protected GameObject player;
         protected Vector3 target;
 
-		protected virtual void Awake()
+        protected IState State { get; set; }
+
+        protected void ChangeState(IState newState)
+        {
+            State.Exit();
+            State = newState;
+            State.Enter();
+        }
+
+        protected virtual void Awake()
         {
             Agent = GetComponent<NavMeshAgent>();
-            player = GameObject.Find("Player");
         }
     }
 }
--- Assets/Scripts/Enemies/ZombieController.cs
+++ Assets/Scripts/Enemies/ZombieController.cs
@@ -3,21 +3,30 @@ using UnityEngine.TextCore.Text;
 using UnityEngine.AI;
 using UnityEditor.SearchService;
 using Unity.VisualScripting;
+using System;
 
 namespace Game.Enemy
 {
     public class ZombieController : EnemyController
     {
+        private IState _wanderState;
+        private IState _chaseState;
+        private IState _investigateState;
+
 		// Start is called once before the first execution of Update after the MonoBehaviour is created
 		void Start()
         {
-            
+            // _wanderState = new WanderState();
+            _chaseState = new ChaseState(this.Agent, GameObject.Find("Player"));
+            // _investigateState = new InvestigateState();
+
+            State = _chaseState;
         }
 
         // Update is called once per frame
         void Update()
         {
-            // Agent.SetDestination(player.transform.position);
+            State.Update();
         }
-    }
+	}
 }
