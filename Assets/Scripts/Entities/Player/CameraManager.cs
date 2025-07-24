259bb66bfd93bd002fdff433ec96f401fbe51714
Aidan Isner <54968155+IsnerA@users.noreply.github.com>
2025-07-24 02:48:49 -0400

    Automatically calculate camera projection matrix

--- Assets/Scripts/Entities/Player/CameraManager.cs
+++ Assets/Scripts/Entities/Player/CameraManager.cs
@@ -1,17 +1,17 @@
-using System;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
     public Camera Camera { get; private set; }
-    [SerializeField] private Matrix4x4 projectionMatrix;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Camera = GetComponent<Camera>();
-        Matrix4x4 p = Camera.projectionMatrix;
-        p.m11 = 0.0883883476483f; // Scale vertical axis of the viewport by a factor of sqrt(2) to account for camera angle
-        Camera.projectionMatrix = p;
+        Vector4 x = new(1 / (Camera.aspect * Camera.orthographicSize), 0, 0, 0);
+        Vector4 y = new(0, Mathf.Sqrt(2) / Camera.orthographicSize, 0, 0);
+        Vector4 z = new(0, 0, -2 / (Camera.farClipPlane - Camera.nearClipPlane), 0);
+        Vector4 w = new(0, 0, -(Camera.farClipPlane + Camera.nearClipPlane) / (Camera.farClipPlane - Camera.nearClipPlane), 1);
+        Camera.projectionMatrix = new Matrix4x4(x, y, z, w);
     }
 }
